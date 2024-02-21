086f27f2ba2bd2cee9395596f5d3c6b62f9dd750
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-02-21 06:10:25 +0200

    create note item

---
+++ lib/views/widgets/note_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: const Color(0xffFFD385),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            contentPadding: const EdgeInsets.all(0),
+            title: const Text(
+              'Flutter Tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'Build your career with Abdelrahman Elbattawy',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 24,
+              ),
+            ),
+          ),
+          Text(
+            'May 21, 2022',
+            style: TextStyle(
+              color: Colors.black.withOpacity(0.4),
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
