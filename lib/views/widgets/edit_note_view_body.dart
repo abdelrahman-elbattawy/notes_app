8d436cb422e24efd68f1054d712610b416a79e84
Abdelrhman Elbattawy <abdo.elbattawy1st@gmail.com>
2024-02-23 05:19:39 +0200

    edit note view done

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -4,6 +4,7 @@ import 'package:notes_app/cubits/notes_cubit/notes_cubit_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
+import 'package:notes_app/views/widgets/edit_note_colors_list.dart';
 
 class EditNoteViewBody extends StatefulWidget {
   const EditNoteViewBody({super.key, required this.noteModel});
@@ -56,6 +57,12 @@ class _EditNoteViewBodyState extends State<EditNoteViewBody> {
             onChanged: (value) => content = value,
             hintText: widget.noteModel.subTitle,
             maxLines: 5,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          EditNoteColorList(
+            note: widget.noteModel,
           ),
         ],
       ),
